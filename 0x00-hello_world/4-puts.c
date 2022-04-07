fc552899e142dee388c8ee9be26f086a9701c6d1
marieloujo <detchenoujoan@gmail.com>
2022-04-07 14:13:30 -0700

    C program that use puts to prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/*
- *that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- * Use the function puts
- * You are not allowed to use printf
- * Your program should end with the value 0
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
