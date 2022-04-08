504bdf3cfe52e3190ca422d75549195b036594a9
marieloujo <detchenoujoan@gmail.com>
2022-04-09 00:26:41 +0100

    Fix betty style

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -32,4 +32,5 @@ int main(void)
 	putchar('\n');
 
 	return (0);
+
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,11 @@ int main(void)
 	int _number;
 
 	for (_number = 0; _number < 10; _number++)
+
 		putchar((_number + '0'));
+
 	putchar('\n');
+
 	return (0);
+
 }
