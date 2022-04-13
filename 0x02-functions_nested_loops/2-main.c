b5d14538e648d3496959a8dad18a09e01acd76f9
marieloujo <detchenoujoan@gmail.com>
2022-04-13 01:58:30 +0100

    All files for this project

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+/**
+ * main - check the code for header School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+/**
+ * main - check the code for header School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    times_table();
+    return (0);
+}
