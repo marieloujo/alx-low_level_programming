5cda1557f6eb7061258bbdc0f20276c437634a06
marieloujo <detchenoujoan@gmail.com>
2022-04-14 17:41:00 +0100

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - check for upper case letter
+ * Return: void
+ */
+void print_numbers(void)
+{
+
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar("\n");
+
+}
