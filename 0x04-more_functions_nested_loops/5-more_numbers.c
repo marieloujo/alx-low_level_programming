5002b78ee2e355a42150cb395540cc841ebbaf27
marieloujo <detchenoujoan@gmail.com>
2022-04-14 18:16:29 +0100

    Fix Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,22 @@
 void more_numbers(void)
 {
 
-	int i = 0, j = 0;
+	int i = 0, j = 0, n;
 
 	for (i = 0; i < 10; i++)
 	{
 
 		for (j = 0; j < 14; j++)
 		{
+			n = j;
 
 			if (j > 9)
 			{
 				_putchar('1');
-				_putchar((j % 10) + '0');
+				n = j % 10;
 			}
-			else
-				_putchar((j + '0'));
+
+			_putchar((n + '0'));
 		}
 
 		_putchar('\n');
