36be3006b774421e4f5d23176a2e963f42f4d467
marieloujo <detchenoujoan@gmail.com>
2022-04-14 17:59:54 +0100

    Fix Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,8 @@ void print_most_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 
-		if (i == 2 || i == 4)
-			break;
-
-		_putchar((i + '0'));
+		if (i != 2 && i != 4)
+			_putchar((i + '0'));
 
 	}
 
