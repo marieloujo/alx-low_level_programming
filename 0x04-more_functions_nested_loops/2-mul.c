148e2718b4d078b678b1e8617132387cb831bc12
marieloujo <detchenoujoan@gmail.com>
2022-04-14 17:34:00 +0100

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a : character to check the case
+ * @b : character to check the case
+ * Return: result of a * b
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
