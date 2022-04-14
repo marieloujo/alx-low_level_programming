56f3b8423459c314c9b88d188c8e5157f3ac8485
marieloujo <detchenoujoan@gmail.com>
2022-04-14 17:10:38 +0100

    Fisk betty style for task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,9 +5,13 @@
  * @c : character to check the case
  * Return:0 or 1
  */
-
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+
+	if (c >= 65 && c <= 90)
+		return (1);
+
+	else
+		return (0);
 
 }
