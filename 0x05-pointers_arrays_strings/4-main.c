84f26f0bb4dc16530b3d4e90e49a827e88e15df3
marieloujo <detchenoujoan@gmail.com>
2022-04-20 18:35:02 +0100

    Fisk Task 4

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
+	str = "Holberton!";
+	print_rev(str);
+	return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
 * print_rev - prints a string, in reverse, followed by a new line.
@@ -11,13 +10,14 @@
 void print_rev(char *s)
 {
 
-	int i = _strlen(s);
+    int i = 0;
+    while (s[i])
+        i++;
 
-	while (i >= 0)
-	{
-		_putchar(*(s + i));
-		i--;
-	}
+    while (i--)
+    {
+        _putchar(s[i]);
+    }
 
 	_putchar('\n');
 
