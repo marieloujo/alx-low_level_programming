99b9a6bfed6749c9918ec866734f642da776c098
marieloujo <detchenoujoan@gmail.com>
2022-04-23 00:19:50 +0100

    Fix code bug

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * _strncpy - a function that copies a string.
@@ -21,6 +22,12 @@ char *_strncpy(char *dest, char *src, int n)
 		if (*(src + i) == '\0')
 			break;
 
+		i++;
+	}
+
+    while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 
