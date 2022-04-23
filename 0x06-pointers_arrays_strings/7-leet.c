eb704d923ba4901e5f272e4bffa53608f0498bc1
marieloujo <detchenoujoan@gmail.com>
2022-04-23 02:14:53 +0100

    Function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,14 +2,37 @@
 #include <string.h>
 
 /**
-* leet - concatenates two strings.
+* leet - a function that encodes a string into 1337.
 *
-* @dest: pointer of char
-* @src: pointer of char
+* @string: pointer of char
 *
 * Return: pointer of char
 */
-char *leet(char *)
+char *leet(char *string)
 {
+
+	int i = 0, j;
+
+	char *_low_case_letters = "aeotl";
+	char *_upper_case_letters = "AEOTL";
+	char *leet = "43071";
+
+	while (*(string + i) != '\0')
+	{
+
+		for (j = 0; j < 5; j++)
+		{
+			if (*(string + i) == _low_case_letters[j] ||
+				*(string + i) == _upper_case_letters[j])
+
+				*(string + i) = leet[j];
+
+		}
+
+		i++;
+
+	}
+
+	return (string);
 
 }
