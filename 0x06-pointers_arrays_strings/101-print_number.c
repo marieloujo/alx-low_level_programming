c1cf3a5436908e33c8031288ba6e0a8f4f66f0fa
marieloujo <detchenoujoan@gmail.com>
2022-04-25 00:30:31 +0100

    Fix task 9 and make task 12

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,14 +10,32 @@
 void print_number(int n)
 {
 
-	if (n < 0)
+	/* if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
 
 
-	_write_number(n);
+	_write_number(n); */
+
+	int divisor = 1, i, resp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+
+	}
 
 }
 
