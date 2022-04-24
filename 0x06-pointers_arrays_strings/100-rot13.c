b7fc92fb431c31df31f4074ef2df8c48c8b5a012
marieloujo <detchenoujoan@gmail.com>
2022-04-24 22:53:22 +0100

    Function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
 * rot13 - a function that encodes a string into  rot13.
 *
@@ -10,25 +10,28 @@
 char *rot13(char *string)
 {
 
-	int i;
-
+	int i, changed = 0;
 
 	for (i = 0; *(string + i) != '\0'; i++)
 	{
+
 		while ((*(string + i) >= 65 && *(string + i) <= 77) ||
 				(*(string + i) >= 97 && *(string + i) <= 109))
 		{
 
 			*(string + i) = (*(string + i) + 13);
+            changed = 1;
+
 			break;
 		}
 
 
-		if ((*(string + i) >= 110 && *(string + i) <= 122) ||
-			(*(string + i) >= 78 && *(string + i) <= 90))
+		if (((*(string + i) >= 110 && *(string + i) <= 122) ||
+			(*(string + i) >= 78 && *(string + i) <= 90)) && !changed)
 
 			*(string + i) = (*(string + i) - 13);
 
+        changed = 0;
 
 	}
 
