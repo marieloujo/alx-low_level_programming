d98c32b2bbf35b8a86e48cb92d3392ca3dcd9969
marieloujo <detchenoujoan@gmail.com>
2022-04-24 22:43:54 +0100

    Fix task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,22 +15,19 @@ char *rot13(char *string)
 
 	for (i = 0; *(string + i) != '\0'; i++)
 	{
-		while ((*(string + i) >= 65 && *(string + i) <= 90) ||
-				(*(string + i) >= 97 && *(string + i) <= 122))
+		while ((*(string + i) >= 65 && *(string + i) <= 77) ||
+				(*(string + i) >= 97 && *(string + i) <= 109))
 		{
 
-			if ((*(string + i) >= 110 && *(string + i) <= 122) ||
-				(*(string + i) >= 78 && *(string + i) <= 90))
-
-				*(string + i) = (*(string + i) - 13);
-
-			/* else
-				*(string + i) = (*(string + i) + 13); */
-
-
+			*(string + i) = (*(string + i) + 13);
 			break;
-
 		}
+
+
+		if ((*(string + i) >= 110 && *(string + i) <= 122) ||
+			(*(string + i) >= 78 && *(string + i) <= 90))
+
+			*(string + i) = (*(string + i) - 13);
 
 
 	}
