b2d7956c984dfff1ceb5f02b2c7d28556bef4c9c
marieloujo <detchenoujoan@gmail.com>
2022-04-25 23:12:26 +0100

    Infinitie add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,66 +1,55 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * infinite_add - add 2 integers.
+ *
  * @n1: integer
  * @n2: integer
  * @r: buffer
- * size_r: size of r
- * Return: char
+ * @size_r: size of r 556236562 56
+ *
+ * Return: pointer char
  */
-
-int _atoi(char *s)
-{
-	int sign = 1, resp = 0, firstNum;
-
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
-
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
-}
-
-void int_to_string(int n)
-{
-	
-	int divisor = 1, i, resp;
-
-
-	for (i = 0; n / divisor > 9; i++)
-	{
-		divisor *= 10;
-	}
-
-	char str[i];
-
-	for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
-	{
-		resp = n / divisor;
-		str[cmpt] = '0' + resp;
-		n = n - resp * divisor;
-	}
-	str[i] = ('0' + n);
-
-}
-
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int sum, a, b;
-	a = _atoi(n1);
-	b = _atoi(n2);
 
-	sum = a + b;
+	int _length_1, _length_2, operande_1, operande_2, retenue = 0, result = 0;
 
+	_length_1 = strlen(n1) - 1;
+	_length_2 = strlen(n2) - 1;
+	r[size_r - 1] = '\0';
+	size_r -= 2;
 
+	while (_length_1 >= 0 || _length_2 >= 0)
+	{
+		if (size_r < 0)
+			return (0);
+
+		operande_1 = _length_1 < 0 ? 0 : (n1[_length_1] - '0');
+		operande_2 = _length_2 < 0 ? 0 : (n2[_length_2] - '0');
+
+		result	    = (operande_1 + operande_2 + retenue);
+		r[size_r]   = ((result % 10) + '0');
+		retenue	    = result / 10;
+
+		_length_1--;
+		_length_2--;
+		size_r--;
+
+	}
+
+	if (retenue != 0)
+	{
+		if (size_r < 0)
+			return (0);
+
+		r[size_r] = retenue + '0';
+		r = &r[(size_r)];
+	}
+	else
+		r = &r[(size_r + 1)];
+
+	return (r);
 }
