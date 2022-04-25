36fbb305efc07ab181c688cc0225d4f659185097
marieloujo <detchenoujoan@gmail.com>
2022-04-25 23:18:32 +0100

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,28 +16,12 @@ void print_number(int n)
 		n *= -1;
 	}
 
-
-	_write_number(n);
-
-}
-
-
-/**
-* _write_number - a function that prints an integer.
-*
-* @n: integer we want to print
-*
-* Return: void
-*/
-void _write_number(int n)
-{
-
-	if ((n + '0') >= 48 && (n + '0') <= 57)
+	if (n < 10)
 		_putchar(n + '0');
 
 	else
 	{
-		_write_number(n / 10);
+		print_number(n / 10);
 		_putchar((n % 10) + '0');
 
 	}
