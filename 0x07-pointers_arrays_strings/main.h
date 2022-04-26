6134a19fc77b66912fa5830ef31f8284d1b92783
marieloujo <detchenoujoan@gmail.com>
2022-04-26 20:25:38 +0100

    Function that prints the chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -9,3 +11,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *, char *);
+
+void print_chessboard(char (*a)[8]);
