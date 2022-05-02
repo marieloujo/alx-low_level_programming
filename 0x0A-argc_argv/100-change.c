ee4aaa303a71c8162a6058bc32e42819514359f2
marieloujo <detchenoujoan@gmail.com>
2022-05-02 21:22:10 +0100

    Initialize 0x0A project

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main -  program that prints its name, followed by a new line
+*
+* @argc: size of argv
+* @argv: list of program parameters
+*
+* Return: Allways 0
+*/
+int main(int argc, int argv[])
+{
+    return (0);
+}
