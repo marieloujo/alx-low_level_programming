00d2a8f4d1bb4a1fe2b79001f2274756ba10c753
marieloujo <detchenoujoan@gmail.com>
2022-05-10 00:26:45 +0100

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,31 +14,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    unsigned int nameLength, ownerLength;
+	unsigned int nameLength, ownerLength;
 	dog_t *myDog = malloc(sizeof(dog_t));
 
 	if (myDog == 0)
 		return (NULL);
 
-    nameLength = strlen(name);
-    ownerLength = strlen(owner);
+	nameLength = strlen(name);
+	ownerLength = strlen(owner);
 
-    myDog->name = malloc(sizeof(char) * nameLength +1);
+	myDog->name = malloc(sizeof(char) * nameLength +1);
 
 	if (myDog->name == 0)
-    {
-        free(myDog);
+	{
+		free(myDog);
 		return (NULL);
-    }
+	}
 
-    myDog->owner = malloc(sizeof(char) * ownerLength +1);
+	myDog->owner = malloc(sizeof(char) * ownerLength +1);
 
-    if (myDog->owner == 0)
-    {
-        free(myDog);
-        free(myDog->name);
+	if (myDog->owner == 0)
+	{
+		free(myDog);
+		free(myDog->name);
 		return (NULL);
-    }
+	}
 
 	myDog->name = strcpy(myDog->name, name);
 	myDog->age = age;
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog -  frees dogs
@@ -10,4 +11,10 @@
 void free_dog(dog_t *d)
 {
 
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
