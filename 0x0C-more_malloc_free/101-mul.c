4654fbaeecf03788b9816756d29b5d0c6f8a394a
marieloujo <detchenoujoan@gmail.com>
2022-05-09 16:45:27 +0100

    Program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,28 +7,15 @@
  * @n: number to be printed
  * Return:void
  */
-void print_number(int n)
+void print_number(unsigned long int n)
 {
 
-	unsigned int n1 = 0;
-
-	if  (n < 0)
+	if (n / 10)
 	{
-		n1 = -n;
-		_putchar('-');
+		print_number(n / 10);
 	}
 
-	else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	_putchar((n % 10) + '0');
 
 }
 
