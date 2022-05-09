33c3ba1dbfb62267e1a6511fda26c4af6fc6a82e
marieloujo <detchenoujoan@gmail.com>
2022-05-10 00:22:13 +0100

    Review new_dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * new_dog - check the code
@@ -13,15 +14,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
+    unsigned int nameLength, ownerLength;
 	dog_t *myDog = malloc(sizeof(dog_t));
 
 	if (myDog == 0)
 		return (NULL);
 
-	myDog->name = name;
+    nameLength = strlen(name);
+    ownerLength = strlen(owner);
+
+    myDog->name = malloc(sizeof(char) * nameLength +1);
+
+	if (myDog->name == 0)
+    {
+        free(myDog);
+		return (NULL);
+    }
+
+    myDog->owner = malloc(sizeof(char) * ownerLength +1);
+
+    if (myDog->owner == 0)
+    {
+        free(myDog);
+        free(myDog->name);
+		return (NULL);
+    }
+
+	myDog->name = strcpy(myDog->name, name);
 	myDog->age = age;
-	myDog->owner = owner;
+	myDog->owner = strcpy(myDog->owner, owner);
 
 	return (myDog);
 
