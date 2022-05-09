095b85e9d72769168ef24b30cc5b0027200f1180
marieloujo <detchenoujoan@gmail.com>
2022-05-09 16:35:22 +0100

    Program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,37 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
+ */
+void print_number(int n)
+{
+
+	unsigned int n1 = 0;
+
+	if  (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+
+}
+
 
 /**
  * main - print the result of the multiplication, followed by a new line
@@ -8,7 +39,6 @@
  * @argv: list
  * Return: 0
  */
-
 int main(int argc, char const *argv[])
 {
 
@@ -24,7 +54,8 @@ int main(int argc, char const *argv[])
 
 	n = atoi(argv[1]) * atoi(argv[2]);
 
-	printf("%lu\n", n);
+	print_number(n);
+	_putchar('\n');
 
 	return (0);
 
