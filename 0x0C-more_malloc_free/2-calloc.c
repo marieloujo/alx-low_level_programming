5c4ffabfe11db47efe310d0c2dec7636dc95f142
marieloujo <detchenoujoan@gmail.com>
2022-05-09 15:15:52 +0100

    Function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _calloc - Function that allocates memory for an array, using malloc
+*
+*@nmemb: lenght of array
+*@size: size of byte
+*
+* Return: pointer of concatenate concanet strings
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+
+	void *calloc;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	calloc = malloc(nmemb * size);
+
+	if (calloc == 0)
+		return (NULL);
+
+	memset(calloc, 0, nmemb * size);
+
+
+	return (calloc);
 
 }
