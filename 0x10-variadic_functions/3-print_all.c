85b5ecb3a360bc9479ff6fcb29611e191d87f49f
marieloujo <detchenoujoan@gmail.com>
2022-05-31 23:51:14 +0100

    Function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,55 @@
 #include "variadic_functions.h"
+#include <string.h>
+
+/**
+ * print_int - Function that prints int
+ *
+ * @list: value we want to print
+ *
+ * Return: void
+ */
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_float - Function that prints float
+ *
+ * @list: value we want to print
+ *
+ * Return: void
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_char - Function that prints char
+ *
+ * @list: value we want to print
+ *
+ * Return: void
+ */
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_str - Function that prints string
+ *
+ * @list: value we want to print
+ *
+ * Return: void
+ */
+void print_str(va_list list)
+{
+	char *value = va_arg(list, char*);
+
+	value == NULL ? printf("(nil)") : printf("%s", value);
+}
 
 /**
  * print_all - Function that prints anything
@@ -9,5 +60,39 @@
  */
 void print_all(const char * const format, ...)
 {
+
+	va_list list;
+	size_t i = 0, length = strlen(format);
+	int j;
+
+	print_anything myPrinter[] = {
+		{"i", print_int},
+		{"c", print_char},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(list, format);
+
+	while (i < length)
+	{
+
+		j = 0;
+
+		while (myPrinter[j].type != NULL && *(myPrinter[j].type) != format[i])
+			j++;
+
+		if (myPrinter[j].type != NULL)
+		{
+			myPrinter[j].printer(list);
+
+			i != (length - 1) ? printf(", ") : printf("\n");
+		}
+
+		i++;
+	}
+
+	va_end(list);
 
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,4 +12,18 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+void print_int(va_list list);
+
+void print_float(va_list list);
+
+void print_char(va_list list);
+
+void print_str(va_list list);
+
+typedef struct print_anything
+{
+    char *type;
+    void (*printer)(va_list);
+} print_anything;
+
 #endif
