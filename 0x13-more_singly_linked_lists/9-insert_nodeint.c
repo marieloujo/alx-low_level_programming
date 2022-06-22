794d6bb3c10a492c157e4a6ffc49085c607ebde9
marieloujo <detchenoujoan@gmail.com>
2022-06-22 13:07:55 +0100

    Update task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head != NULL)
 	{
-		if (*head == NULL)
+		if (*head == NULL && idx == 0)
 		{
 			new = add_nodeint_end(head, n);
 
