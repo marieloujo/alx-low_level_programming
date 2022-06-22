c1063459401c29c7f9065bbd0bcb74da125a09d5
marieloujo <detchenoujoan@gmail.com>
2022-06-22 13:37:24 +0100

    Function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
+/**
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+
+	newlist[i] = new;
+	free(list);
+
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - Function that prints a listint_t linked list
+ * This function can print lists with a loop
+ * You should go through the list only once
+ * If the function fails, exit the program with status 98
+ *
+ * @head:  the number of nodes in the list
+ *
+ * Return: a pointer to the first node of the reversed list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-    
+	size_t i, num = 0;
+	const listint_t **list = NULL;
+
+	while (head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+
+	free(list);
+
+	return (num);
 }
