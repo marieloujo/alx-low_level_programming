e85d543720e8ffcf5922768806a314a5c52be028
marieloujo <detchenoujoan@gmail.com>
2022-07-05 19:29:58 +0100

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,2 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters);
+/**
+ * read_textfile - reads a text file and prints it t
+ * the POSIX standard output
+ * @filename: path of file
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char *buffer;
+	ssize_t read_letters, write_letters;
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_letters = read(file, buffer, letters);
+	if (read_letters == -1)
+		return (0);
+
+	write_letters = write(STDOUT_FILENO, buffer, read_letters);
+
+	if (write_letters == -1 || read_letters != write_letters)
+		return (0);
+
+	free(buffer);
+	close(file);
+
+	return (write_letters);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
@@ -5,3 +7,4 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+#endif
