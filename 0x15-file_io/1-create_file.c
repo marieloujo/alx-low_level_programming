ca04fddf54e7ffc81a79a4e70f0b684d8116fab5
marieloujo <detchenoujoan@gmail.com>
2022-07-05 19:53:09 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,40 @@
-int create_file(const char *filename, char *text_content);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * create_file - reads a text file and prints it t
+ * the POSIX standard output
+ * @filename: path of file
+ * @text_content: string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd, stat;
+
+    if (filename == NULL)
+        return (-1);
+
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+    if (fd == -1)
+        return (-1);
+
+    if (text_content)
+    {
+        stat = write(fd, text_content, strlen(text_content));
+
+        if (stat == -1)
+            return (-1);
+    }
+
+    close(fd);
+
+    return (1);
+}
