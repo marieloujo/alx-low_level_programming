e18086d5e565194a31f0d77a3a939d87bf80d05d
marieloujo <detchenoujoan@gmail.com>
2022-07-17 14:46:18 +0100

    frees a dlistint_t list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,9 +6,17 @@
 /**
  * sum_dlistint -  sum of all the data (n) of a dlistint_t linked list
  * @head: head of the dlistint_t list
- * Return: the sum of all the data (n) 
+ * Return: the sum of all the data (n)
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
 }
